628915a5e54fbee6894cc4a4756b1f3afa0b2f5f
tategakibunko <lambda.watanabe@gmail.com>
2016-01-17 08:45:36 +0900

    update fa plugin

--- js/nehan.style.js
+++ js/nehan.style.js
@@ -10,6 +10,31 @@ Nehan.setStyles({
 });
 
 // font-awesome icon
+Nehan.addSingleTagName("fa");
+Nehan.setStyle("fa", {
+  display:"inline",
+  width:"1em",
+  height:"1em",
+  oncreate:function(ctx){
+    var $i = document.createElement("i");
+    var names = ctx.box.context.style.markup.getAttr("name").replace(/\s+/g, " ").split(" ");
+    var icon_names = ["fa"];
+    for(var i = 0; i < names.length; i++){
+      icon_names.push("fa-" + names[i]);
+    }
+    $i.className = icon_names.join(" ");
+    ctx.dom.appendChild($i);
+    if(ctx.box.context.style.isTextVertical()){
+      ctx.dom.style.textAlign = "center";
+    }
+  },
+  onload:function(ctx){
+    ctx.getMarkup().setAttr("lazy", true);
+  }
+});
+
+/* (for ver<= 5.4.0)
+// font-awesome icon
 Nehan.LexingRule.addSingleTagByName("fa");
 Nehan.setStyle("fa", {
   display:"inline",
@@ -25,4 +50,4 @@ Nehan.setStyle("fa", {
     markup.setAttr("pasted", true);
   }
 });
-
+*/
